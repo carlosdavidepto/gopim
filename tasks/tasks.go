7e9c222ad4df32f976206bc34883b913415aae63
Carlos Davide <carlosdavidepto@13brane.net>
2016-06-24 21:14:24 -0400

    added Del and Do methods

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -1,34 +1,58 @@
 package tasks
 
 import (
-  "strings"
+	"strings"
 )
 
 type DB struct {
-  tasks []string
+	tasks []string
 }
 
 func (d *DB) Unpack(tasks string) {
-  tmp := strings.Split(tasks, "\n")
-  
-  for i := len(tmp) - 1; i >= 0; i-- {
-    if (tmp[i] != "") {
-      break
-    }
-    tmp = tmp[:i]
-  }
+	tmp := strings.Split(tasks, "\n")
 
-  d.tasks = tmp
+	for i := len(tmp) - 1; i >= 0; i-- {
+		if tmp[i] != "" {
+			break
+		}
+		tmp = tmp[:i]
+	}
+
+	d.tasks = tmp
 }
 
 func (d *DB) Pack() string {
-  return strings.Join(d.tasks, "\n") + "\n"
+	return strings.Join(d.tasks, "\n") + "\n"
 }
 
 func (d *DB) Len() int {
-  return len(d.tasks)
+	return len(d.tasks)
 }
 
 func (d *DB) Add(task string) {
-  d.tasks = append(d.tasks, task)
+	d.tasks = append(d.tasks, task)
+}
+
+func (d *DB) Del(id int) {
+	realid := id - 1
+
+	if realid < 0 || realid > len(d.tasks)-1 {
+		return
+	}
+
+	d.tasks = append(d.tasks[:realid], d.tasks[(realid+1):]...)
+}
+
+func (d *DB) Do(id int) {
+	realid := id - 1
+
+	if realid < 0 || realid > len(d.tasks)-1 {
+		return
+	}
+
+	if d.tasks[realid][:2] == "x " {
+		return
+	}
+
+	d.tasks[realid] = "x " + d.tasks[realid]
 }
--- tasks/tasks_test.go
+++ tasks/tasks_test.go
@@ -1,54 +1,90 @@
 package tasks
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestEmptyStringHasNoTasks(t *testing.T) {
-  db := &DB{}
-  db.Unpack("")
-  if db.Len() != 0 {
-    t.Errorf("empty database should not contain tasks")
-  }
+	db := &DB{}
+	db.Unpack("")
+	if db.Len() != 0 {
+		t.Errorf("empty database should not contain tasks")
+	}
 }
 
-func TestOneLineIsOneTask(t * testing.T) {
-  db := &DB{}
-  db.Unpack("one simple task")
-  if db.Len() != 1 {
-    t.Errorf("one line should correspond to one task")
-  }
+func TestOneLineIsOneTask(t *testing.T) {
+	db := &DB{}
+	db.Unpack("one simple task")
+	if db.Len() != 1 {
+		t.Errorf("one line should correspond to one task")
+	}
 }
 
 func TestEmptyLinesAtEndIgnored(t *testing.T) {
-  db := &DB{}
-  db.Unpack("task\n\n\n")
-  if db.Len() != 1 {
-    t.Errorf("empty lines at the end should be ignored")
-  }
+	db := &DB{}
+	db.Unpack("task\n\n\n")
+	if db.Len() != 1 {
+		t.Errorf("empty lines at the end should be ignored")
+	}
 }
 
 func TestEmptyLinesInTheMiddleAreKept(t *testing.T) {
-  db := &DB{}
-  db.Unpack("t\n\nt\n")
-  if db.Len() != 3 {
-    t.Errorf("empty lines in the middle should be ignored")
-  }
+	db := &DB{}
+	db.Unpack("t\n\nt\n")
+	if db.Len() != 3 {
+		t.Errorf("empty lines in the middle should be ignored")
+	}
 }
 
 func TestPackNoModification(t *testing.T) {
-  db := &DB{}
-  db.Unpack("task1\n")
-  if db.Pack() != "task1\n" {
-    t.Errorf("Pack() modifies db in unexpected way")
-  }
+	db := &DB{}
+	db.Unpack("task1\n")
+	if db.Pack() != "task1\n" {
+		t.Errorf("Pack() modifies db in unexpected way")
+	}
 }
 
 func TestAddTask(t *testing.T) {
-  db := &DB{}
-  db.Unpack("task1\n")
-  db.Add("task2")
-  if db.Pack() != "task1\ntask2\n" {
-    t.Errorf("Pack() modifies db in unexpected way")
-  } 
+	db := &DB{}
+	db.Unpack("task1\n")
+	db.Add("task2")
+	if db.Pack() != "task1\ntask2\n" {
+		t.Errorf("Add() does not add a task correctly")
+	}
+}
+
+func TestDelTaskNonexistent(t *testing.T) {
+	db := &DB{}
+	db.Unpack("\n")
+	db.Del(23)
+	if db.Pack() != "\n" {
+		t.Errorf("Del() does not ignore nonexistent tasks")
+	}
+}
+
+func TestDelTask(t *testing.T) {
+	db := &DB{}
+	db.Unpack("task1\n")
+	db.Del(1)
+	if db.Pack() != "\n" {
+		t.Errorf("Del() does not delete a task correctly")
+	}
+}
+
+func TestDoTaskNoEffect(t *testing.T) {
+	db := &DB{}
+	db.Unpack("x task1\n")
+	db.Do(1)
+	if db.Pack() != "x task1\n" {
+		t.Errorf("Do() does not ignore done tasks")
+	}
+}
+
+func TestDoTask(t *testing.T) {
+	db := &DB{}
+	db.Unpack("task1\n")
+	db.Do(1)
+	if db.Pack() != "x task1\n" {
+		t.Errorf("Do() does not mark tasks as done correctly")
+	}
 }
